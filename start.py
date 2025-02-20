747a3d26e2082f8e5329ac52a8987c9433215988
kabatyy <sam.kabati@gmail.com>
2025-02-20 13:06:08 +0300

    add scripts to build vocabulary and load data

--- start.py
+++ start.py
@@ -29,9 +29,8 @@ def extract_images(train_zip_path, val_zip_path, test_zip_path, annotations_trai
     os.makedirs(test_extract_dir, exist_ok=True)
     val_extract_dir = os.path.join(COCO_API_DIR, "images/val2014")
     os.makedirs(val_extract_dir, exist_ok=True)
-    annotations_extract_dir = os.path.join(COCO_API_DIR, "annotations/annotations")
-    os.makedirs(annotations_extract_dir, exist_ok=True)
-        
+    annotations_extract_dir = COCO_API_DIR
+
     subprocess.run(["unzip", "-o", train_zip_path, "-d", train_extract_dir], check=True)
     subprocess.run(["unzip", "-o", val_zip_path, "-d", val_extract_dir], check=True)
     subprocess.run(["unzip", "-o", test_zip_path, "-d", test_extract_dir], check=True)
